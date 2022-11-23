50d2d2a3b09d2e7d45d6a6d09dd82ea7acd8f1a0
lmarzocchetti <61746163+lmarzocchetti@users.noreply.github.com>
2022-11-23 20:39:28 +0100

    Add files via upload

---
+++ mangaworld_downloader.py
@@ -0,0 +1,257 @@
+import sys
+import requests
+import bs4
+import os
+import subprocess
+import shutil
+import pathlib
+import threading
+from PIL import Image
+import img2pdf
+
+RESEARCH_STRING = "https://www.mangaworld.so/archive?keyword="
+
+CHAPTERS_STRING = "?style=pages"
+
+
+def research_manga(manga: str) -> dict[str, str]:
+    """
+    Function that take a manga name and return 
+    the dictionary of all results
+
+    Args:
+        manga (str): string of the researched manga
+
+    Returns:
+        dict[str, str]: dictionary of manga and their link
+    """
+    page = requests.get(RESEARCH_STRING + manga)
+    soup = bs4.BeautifulSoup(page.content, "html.parser")
+    results = soup.find("body")
+    job_all = results.find_all("a", class_="thumb position-relative")
+
+    manga_dict = {job.__str__().split(r'"')[5]: job.__str__().split(r'"')[3] for job in job_all}
+
+    return manga_dict
+
+
+def manga_with_volumes_links(job_all: bs4.element.ResultSet) -> dict[str, list[str]]:
+    vol_chap_dict = {}
+
+    for num_vol, vol in enumerate(job_all):
+        for chap in vol.find_all("a", class_="chap"):
+            key = f"Volume{num_vol}"
+            if key not in vol_chap_dict.keys():
+                vol_chap_dict[key] = []
+            vol_chap_dict[key].append(chap.__str__().split(r'"')[3])
+
+    for volume, chap_list in vol_chap_dict.items():
+        tmp = chap_list[::-1]
+        vol_chap_dict[volume] = tmp
+
+    return vol_chap_dict
+
+
+def manga_with_chapters_links(job_all: bs4.element.ResultSet) -> dict[str, list[str]]:
+    vol_chap_dict = {}
+    vol_chap_dict["Volume0"] = []
+
+    for _, vol in enumerate(job_all):
+        for chap in vol.find_all("a", class_="chap"):
+            vol_chap_dict["Volume0"].append(
+                chap.__str__().split(r'"')[3].split("?")[0])
+
+    return vol_chap_dict
+
+
+def volumes_with_chapter_link(manga_url: str) -> dict[str, list[str]]:
+    """
+    Function that take a manga url and return a
+    dictionary with all volumes and link of their chapters
+
+    Args:
+        manga_url (str): String of a manga url
+
+    Returns:
+        dict[str, list[str]]: dictionary with in key a string with volume name, and value a list with all the links of every chapters in volumes
+    """
+    page = requests.get(manga_url)
+    soup = bs4.BeautifulSoup(page.content, "html.parser")
+    results = soup.find("body")
+    job_all = results.find_all("div", class_="volume-element pl-2")[::-1]
+
+    # se un manga non e' diviso in volumi, ma contiene solo capitoli
+    if (len(job_all) == 0):
+        job_all = results.find_all("div", class_="chapter pl-2")[::-1]
+        return manga_with_chapters_links(job_all)
+    else:
+        return manga_with_volumes_links(job_all)
+
+
+def print_manga(manga_dict: dict[str, str]):
+    """Clear the screen and print all mangas available
+
+    Args:
+        manga_dict (dict[str, str]): Mangas researched
+    """
+    _ = subprocess.call('clear' if os.name == 'posix' else 'cls')
+
+    for index, manga in enumerate(manga_dict.keys()):
+        print(f"{index}-{manga}")
+    print("\n")
+
+
+def choose_manga(manga_dict: dict[str, str]) -> str:
+    """Simple TUI for select a manga to download
+
+    Args:
+        manga_dict (dict[str, str]): Mangas researched
+
+    Returns:
+        str: Manga selected
+    """
+    while True:
+        print_manga(manga_dict)
+        selected_manga: str = input(
+            "Insert the number of the manga do you want to download: ")
+
+        if selected_manga.isdigit() and int(selected_manga) >= 0 and int(selected_manga) < len(manga_dict.keys()):
+            return list(manga_dict.keys())[int(selected_manga)]
+
+
+def number_of_images_in_chapter(chapter_url: str) -> int:
+    page = page = requests.get(chapter_url)
+    soup = bs4.BeautifulSoup(page.content, "html.parser")
+    results = soup.find("body")
+    job = results.find(
+        "select", class_="page custom-select").find("option").__str__()
+
+    number_of_images = job.split("/")[1].split("<")[0]
+
+    return number_of_images
+
+
+def download_image(image_url: str, vol_index: str, chap_index: str, image_index: str, selected_manga: str) -> None:
+    page = requests.get(image_url)
+    soup = bs4.BeautifulSoup(page.content, "html.parser")
+    results = soup.find("body")
+    image_link = results.find(
+        "div", class_="col-12 text-center position-relative").find("img", class_="img-fluid").get("src")
+
+    image = requests.get(image_link, stream=True)
+    if image.status_code == 200:
+        image.raw.decode_content = True
+    with open(os.path.join("Data", selected_manga, str(vol_index), f"{chap_index}_{image_index}.jpg"), "wb") as f:
+        shutil.copyfileobj(image.raw, f)
+
+
+def download_chapter_images(chapter_url: str, vol_index: str, chap_index: str, selected_manga: str, number_of_images: int) -> None:
+    threads = []
+
+    for i in range(1, int(number_of_images) + 1):
+        url: str = chapter_url + "/" + str(i) + CHAPTERS_STRING
+        threads.append(threading.Thread(target=download_image, args=(
+            url, vol_index, chap_index, str(i), selected_manga)))
+
+    for thread in threads:
+        thread.start()
+
+    for i in range(len(threads)):
+        threads[i].join()
+
+
+def download_volumes_images(vol_chap_dict: dict[str, list[str]], selected_manga: str) -> dict[int, dict[int, int]]:
+    vol_chap_num_images_dict: dict[int, dict[int, int]] = {}
+
+    for index_vol, vol_name in enumerate(vol_chap_dict.keys()):
+        chap_num_pages_dict: dict[int, int] = {}
+
+        for index_chap, chap_link in enumerate(vol_chap_dict[vol_name]):
+            url: str = chap_link + "/" + str(1) + CHAPTERS_STRING
+            number_of_images = number_of_images_in_chapter(url)
+            chap_num_pages_dict[index_chap] = number_of_images
+            download_chapter_images(chap_link, str(index_vol), str(index_chap), selected_manga, number_of_images)
+        
+        vol_chap_num_images_dict[index_vol] = chap_num_pages_dict
+
+    return vol_chap_num_images_dict
+
+
+def create_data_volumes_folders(selected_manga: str, vol_chap_dict: dict[str, list[str]]) -> None:
+    """Create Data folder, manga folder and inside create the Volumes folders with "volumeNumber_numberofchapters"
+
+    Args:
+        selected_manga (str): manga which are selected
+        vol_chap_dict (dict[str, list[str]]): dictionary with all volumes and list of links to their chapters
+    """
+    base_dir = os.path.abspath(os.curdir)
+
+    os.mkdir("Data")
+    os.chdir("Data")
+
+    os.mkdir(selected_manga)
+    os.chdir(selected_manga)
+
+    for index, _ in enumerate(vol_chap_dict.keys()):
+        # name = f"{index}_{len(vol_chap_dict[volume])}"
+        os.mkdir(str(index))
+
+    os.chdir(base_dir)
+
+
+def remove_data_folder() -> None:
+    """Remove recursively all data in the Data folder
+    """
+    shutil.rmtree("Data")
+    
+def create_pdf(vol_chap_num_pages: dict[int, dict[int, int]], selected_manga: str) -> None:
+    for vol_num, chap_num_pag_dict in vol_chap_num_pages.items():
+        images = []
+
+        for chap_num, num_pages in chap_num_pag_dict.items():
+            for i in range(1, int(num_pages) + 1):
+                images.append(Image.open(os.path.join("Data",selected_manga, str(vol_num), f"{chap_num}_{i}.jpg")))
+
+
+        images_filename = [image.filename for image in images]
+
+        pdf_bytes = img2pdf.convert(images_filename)
+
+        file = open(f"Volume_{vol_num}.pdf", "wb")
+
+        file.write(pdf_bytes)
+
+        for image in images:
+            image.close()
+        
+        file.close()
+
+def main():
+    if len(sys.argv) != 2:
+        print("Insert a manga to research!", file=sys.stderr)
+        exit(-1)
+
+    manga_to_research: str = sys.argv[1]
+
+    manga_dict: dict[str, str] = research_manga(manga_to_research)
+
+    if len(manga_dict.keys()) == 0:
+        print("No manga found with that name! Closing the program...", file=sys.stderr)
+        exit(-2)
+
+    selected_manga: str = choose_manga(manga_dict)
+
+    vol_chap_dict: dict[str, list[str]] = volumes_with_chapter_link(
+        manga_dict[selected_manga])
+
+    create_data_volumes_folders(selected_manga, vol_chap_dict)
+
+    volume_chap_num_pages_dict: dict[int, dict[int, int]] = download_volumes_images(vol_chap_dict, selected_manga)
+
+    create_pdf(volume_chap_num_pages_dict, selected_manga)
+
+    remove_data_folder()
+
+
+if __name__ == "__main__":
+    main()
